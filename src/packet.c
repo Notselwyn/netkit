6f84d7106b17229c3d7081d15c076912693d8ac7
Your Name <you@example.com>
2023-06-30 11:42:44 -0400

    fix: memory bug (OOB write)

--- src/packet.c
+++ src/packet.c
@@ -10,13 +10,16 @@ void packet_destructor(struct kref *ref)
 {
     packet_t *packet = container_of(ref, packet_t, refcount);
 
+    pr_err("[*] destructing packet...");
+
+    // destruct content if it exists
     if (packet->content)
     {
-        memset(packet->content, '\x00', packet->content_len);
+        //memset(packet->content, '\x00', packet->content_len);
         kfree(packet->content);
     }
 
-    memset(packet, '\x00', sizeof(*packet));
+    //memset(packet, '\x00', sizeof(*packet));
     kfree(packet);
 }
 
@@ -29,22 +32,25 @@ packet_t *packet_init(const struct raw_packet *buffer, size_t count)
     packet_t *packet;
     size_t packet_header_len;
 
+    // do size check
     packet_header_len = sizeof(packet->password) + sizeof(packet->command);
     if (count < packet_header_len || count - packet_header_len > CONTENT_MAX_LEN)
         return ERR_PTR(-EMSGSIZE);
 
-    packet = kcalloc(sizeof(packet), 1, GFP_KERNEL);
+    // allocate memory
+    packet = kcalloc(sizeof(*packet), 1, GFP_KERNEL);
     if (!packet)
         return ERR_PTR(-ENOMEM);
     
     // set fields manually to allow for internal structure packet reorganisation
     memcpy(packet->password, buffer->password, sizeof(packet->password));
     packet->command = buffer->command;
-    packet->content_len = count - packet_header_len;
+    packet->content_len = count - packet_header_len + 1;
 
-    if (packet->content_len > 0)
+    // create content if necessary, doing +1 for extra nullbyte
+    if (packet->content_len-1 > 0)
     {
-        packet->content = kcalloc(packet->content_len, 1, GFP_KERNEL);
+        packet->content = kcalloc(packet->content_len, 1, GFP_KERNEL);  // +1 for str
         if (!packet->content)
         {
             memset(&packet, '\x00', sizeof(*packet));
@@ -54,10 +60,9 @@ packet_t *packet_init(const struct raw_packet *buffer, size_t count)
             return ERR_PTR(-ENOMEM);
         }
 
-        memcpy(packet->content, &buffer->content, packet->content_len);
+        memcpy(packet->content, &buffer->content, packet->content_len-1);
     }
 
-    
     kref_init(&packet->refcount);
 
     return packet;
